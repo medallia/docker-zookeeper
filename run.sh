c5a3900069f19bd3bba650ac2392913f7381e359
JC Martin <jcmartin@medallia.com>
2016-05-08 09:19:00 -0700

    Add more safeguards

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ set -x
 
 NET_IP="$(ip route get 8.8.8.8| head --lines 1 | sed -r -e 's/.+src ([^ ]+).*$/\1/')"
 
-if [[ -z $DATADIR || -z $ZK_HOSTS ]]; then
-    echo "Missing parameters"
+if [[ -z $ZK_HOSTS ]]; then
+    echo "Missing ZK_HOSTS parameter"
     exit 1
 fi
 
@@ -14,7 +14,7 @@ IFS=',' read -a HOSTS <<< "${ZK_HOSTS}"
 mkdir -p $DATADIR/conf
 chmod -R a+rwx $DATADIR/conf
 
-cat > $DATADIR/conf/zoo.cfg << EOF
+cat > $DATADIR/conf/zoo.cfg-temp << EOF
 tickTime=2000
 dataDir=${DATADIR}
 clientPort=${CLIENTPORT}
@@ -27,15 +27,22 @@ counter=1
 MY_ID=""
 for i in "${HOSTS[@]}"; do 
   IFS=: read -r ip port <<< $i 
-  echo "server.$counter=${ip}:2888:3888" >> $DATADIR/conf/zoo.cfg
+  echo "server.$counter=${ip}:2888:3888" >> $DATADIR/conf/zoo.cfg-temp
   if [ "$ip" = "$NET_IP" ]; then
       MY_ID=$counter
   fi
   (( counter ++ ))
 done
 
+if [[ -z $MY_ID ]]; then
+    echo "Cannot find $NET_IP in $ZK_HOSTS"
+    exit 1
+fi
+
 cat > $DATADIR/myid << END
 $MY_ID
 END
+
+mv $DATADIR/conf/zoo.cfg-temp $DATADIR/conf/zoo.cfg
 
 exec /usr/share/zookeeper/bin/zkServer.sh "$@"
